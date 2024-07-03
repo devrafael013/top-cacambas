01a3626fd1aed70865d97ebbb42e10ce49b9c8be
Rafael Silva <devrafael013@gmail.com>
2024-07-02 22:06:59 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,99 @@
+$( document ).ready(function() {
+
+  // Filtro portfólio
+
+  $('.filter-btn').on('click', function() {
+
+    let type = $(this).attr('id');
+    let boxes = $('.project-box');
+
+    $('.main-btn').removeClass('active');
+    $(this).addClass('active');
+
+    if(type == 'dsg-btn') {
+      eachBoxes('dsg', boxes);
+    } else if(type == 'dev-btn') {
+      eachBoxes('dev', boxes);
+    } else if(type == 'seo-btn') {
+      eachBoxes('seo', boxes);
+    } else {
+      eachBoxes('all', boxes);
+    }
+
+  });
+
+  function eachBoxes(type, boxes) {
+
+    if(type == 'all') {
+      $(boxes).fadeIn();
+    } else {
+      $(boxes).each(function() {
+        if(!$(this).hasClass(type)) {
+          $(this).fadeOut('slow');
+        } else {
+          $(this).fadeIn();
+        }
+      });
+    }
+  }
+
+  // scroll para as seções
+
+  let navBtn = $('.nav-item');
+
+  let bannerSection = $('#mainSlider');
+  let aboutSection = $('#about-area');
+  let servicesSection = $('#services-area');
+  let contactSection = $('#contact-area');
+
+  let scrollTo = '';
+
+  $(navBtn).click(function() {
+
+    let btnId = $(this).attr('id');
+
+    if(btnId == 'about-menu') {
+      scrollTo = aboutSection;
+    } else if(btnId == 'services-menu') {
+      scrollTo = servicesSection;
+    } else if(btnId == 'contact-menu') {
+      scrollTo = contactSection;
+    } else {
+      scrollTo = bannerSection;
+    }
+
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(scrollTo).offset().top - 90
+    }, 200);
+  });
+
+});
+
+// parceiros
+
+const scrollers = document.querySelectorAll(".scroller");
+
+// If a user hasn't opted in for recuded motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
